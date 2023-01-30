7a16503bd37e8cdebdd0f2fa7b09a525b31bf758
Pablo Jacome <pjacome@ennube.solutions>
2023-01-30 14:59:14 -0500

    Update translation for quechua and portuguese

--- frontend/assets/translations/portuguese.ts
+++ frontend/assets/translations/portuguese.ts
@@ -4,6 +4,7 @@ const values = {
       chain_placeholder: 'Selecionar cadeia',
       name_placeholder: 'Digite o nome do token',
       submit_button: 'Salvar Token',
+      title: 'Carteira'
     },
     App: {
       select_network_title: 'Redes',
@@ -15,6 +16,14 @@ const values = {
       token_title: 'Carteira',
       view_network_title: 'Rede',
       view_wallet_title: 'Carteira',
+    },
+    ConnectionRequest: {
+      approve_button: 'Aprovar',
+      next_button: 'Avançar',
+      reject_button: 'Rejeitar',
+      rejected: "rejeitado pelo usuário",
+      title: 'Solicitação de Conexão',
+      wallet_select_instructions: 'Selecionar carteira(s)',
     },
     CreateNetwork: {
       chainId_placeholder: 'Digite o ID da cadeia',
@@ -35,15 +44,61 @@ const values = {
         name_entry_button_loadingtext: 'Enviando',
         name_entry_input_placeholder: 'Definir nome da carteira',
       },
+      LegacyEthSign: {
+        approve_button: 'Aprovar',
+        header: 'Pedido de Assinatura',
+        reject_button: 'Rejeitar',
+        title: 'Mensagem de assinatura'
+      },
+      LegacySendTransaction: {
+        amount: 'Valor: ',
+        approve_button: 'Aprovar',
+        from :'De: ',
+        header: 'Assinar transação',
+        reject_button: 'Rejeitar',
+        title: 'Enviar transação',
+        to: 'Para: '
+      },
+      LegacySignTransaction: {
+        approve_button: 'Aprovar',
+        header: 'Assinar transação',
+        reject_button: 'Rejeitar',
+        title: 'Assinar Transação'
+      },
+      LegacySignTypedData: {
+        approve_button: 'Aprovar',
+        header: 'Solicitação de mensagem de assinatura',
+        header_origin: 'Origem: ',
+        reject_button: 'Rejeitar',
+        title: 'Mensagem de assinatura'
+      },
+      Listener: {
+        failure_message: 'Falha na transação',
+        success_message: 'Transação bem-sucedida'
+      },
       Menu: {
         network_button: 'REDES',
+        nft_button: 'NFTS',
         password_button: 'SENHA',
+        qr_scan_button: 'VERREDURA QR',
         wallet_button: 'CARTEIRAS',
       },
       NewWallet: {
         create_button: 'Criar Carteira',
         instructions: "Crie novo ou recupere de mnenomic",
         recover_button: 'Recuperar carteira',
+      },
+      PasswordComponent: {
+        error_message: 'Senha Invalida',
+        form_label: 'Senha ',
+        form_placeholder: 'Insira a Senha',
+        header: 'Senha',
+        submit_button: 'Enviar'
+      },
+      QRReader: {
+        permission_denied: 'Sem acesso à câmera',
+        permission_request: 'Solicitando permissão para câmera',
+        rescan: 'Toque para escanear novamente'
       },
       Qrwallet: {
         instructions: 'Compartilhe para receber fundos'
@@ -67,12 +122,14 @@ const values = {
       SelectNetwork: {
         new_button: 'Nova Rede',
         select_button: 'Selecionar rede',
-        select_button_tooltip: 'Mais opções'
+        select_button_tooltip: 'Mais opções',
+        title: 'Redes'
       },
       SelectWallet: {
         new_button: 'Nova carteira',
         select_button: 'Selecionar carteira',
-        select_button_tooltip: 'Mais opções'
+        select_button_tooltip: 'Mais opções',
+        title: 'Redes'
       },
       SendToken: {
         address_placeholder: 'Digite o endereço do destinatário',
@@ -80,11 +137,60 @@ const values = {
         title: 'Enviar token:',
         token_placeholder: 'Selecionar token',
       },
+      SendTransaction: {
+        amount: 'Valor: ',
+        approve_button: 'Aprovar',
+        from :'De: ',
+        header: 'Assinar transação',
+        reject_button: 'Rejeitar',
+        title: 'Enviar transação',
+        to: 'Para: '
+      },
+      SetPassword: {
+        form_confirmation: 'Confirmar Senha',
+        form_error_text: 'Pelo menos 6 caracteres são obrigatórios.',
+        form_helper_text: 'Deve ter pelo menos 6 caracteres.',
+        form_new_header: 'Senha',
+        form_old_header: 'Senha atual',
+        form_save_button: 'Save',
+        password_placeholder: 'Senha',
+        title: 'Definir Senha'
+      },
+      SignEth: {
+        approve_button: 'Aprovar',
+        header: 'Pedido de Assinatura',
+        reject_button: 'Rejeitar',
+        title: 'Mensagem de assinatura'
+      },
+      SignTransaction: {
+        approve_button: 'Aprovar',
+        header: 'Assinar transação',
+        reject_button: 'Rejeitar',
+        title: 'Mensagem de assinatura'
+      },
+      SignTyped: {
+        approve_button: 'Aprovar',
+        header: 'Solicitação de mensagem de assinatura',
+        header_origin: 'Origem: ',
+        reject_button: 'Rejeitar',
+        title: 'Mensagem de assinatura'
+      },
+      SwapToken: {
+        title: 'Carteira'
+      },
+      TokenItem: {
+        menu_accessiblity_label: "Menu mais opções",
+        send_token_button: 'Enviar'
+      },
+      ViewNetwork: {
+        title: 'Rede'
+      },
       ViewWallet: {
         clear_button: 'Limpar transações',
         holdings_placeholder: 'Inserir Holdings de moedas',
         new_button: 'Adicionar Token',
         tab_list: ['Participações', 'Transações', , 'NFTs'],
+        title: 'Carteira',
         transaction_placeholder: 'Inserir histórico de transações',
       }
     };
