c6e53a8038961b80018c520c51fbed8b5fb5bf33
cristhianarevalo <carevalo@ennube.solutions>
2023-03-03 15:33:33 -0500

    Updated GlobalStyles

---
+++ GlobalStyles.ts
@@ -0,0 +1,41 @@
+/* fonts */
+export const FontFamily = {
+  inter: "Inter",
+  sFProDisplay: "SF Pro Display",
+  sFProText: "SF Pro Text",
+  poppins: "Poppins",
+};
+/* font sizes */
+export const FontSize = {
+  size_2xs: 10,
+  size_xs: 12,
+  size_sm: 13,
+  size_base: 14,
+  size_lg: 16,
+  size_xl: 20,
+  size_2xl: 24,
+  size_3xl: 25,
+  size_4xl: 30,
+};
+/* Colors */
+export const Color = {
+  yellow: "#d4e815",
+  black: "#000",
+  dimgray_100: "#575757",
+  gray_100: "#868686",
+  gray_200: "#202020",
+  gray_300: "#181818",
+  gray_400: "#0b0a0a",
+  white: "#fff",
+  darkgray_100: "#969696",
+  darkgray_200: "#959595",
+  gainsboro: "rgba(217, 217, 217, 0)",
+};
+/* border radiuses */
+export const Border = {
+  br_xs: 5,
+  br_sm: 8,
+  br_md: 12,
+  br_lg: 18,
+  br_xl: 144,
+};
