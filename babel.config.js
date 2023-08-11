77afcf01fb23117c38cbbea8230611fd78afda4d
Jordan Paul <jordan@smartcampaigns.xyz>
2023-08-11 08:48:07 -0700

    hoping to fix compile

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: [
-      'babel-preset-expo',
+      'module:metro-react-native-babel-preset',
       ['@babel/preset-env', {targets: {node: 'current'}}],
       '@babel/preset-typescript',
     ],
--- frontend/pages/walletConnect/v2/Requests.tsx
+++ frontend/pages/walletConnect/v2/Requests.tsx
@@ -161,11 +161,7 @@ export default function Requests({navigation, route}) {
   return (
     <GuestLayout>
       <ScrollView>
-        <Center         
-          _light={{ backgroundColor: Color.white }}
-          _dark={{ backgroundColor: Color.black }}
-          minH={'100%'}
-        >
+        
           <VStack space={5} py={4} w={'full'}>
             {
               requests.map((val, i) => {
@@ -177,7 +173,6 @@ export default function Requests({navigation, route}) {
               })
             }
           </VStack>
-        </Center>
       </ScrollView>
     </GuestLayout>
   );
