8db8480bc835b2b23d1a0332a3dfff95e8e2096c
Jordan Paul <jordan@smartcampaigns.xyz>
2023-08-15 23:36:26 -0700

    try this

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: [
-      'module:metro-react-native-babel-preset',
+      'babel-preset-expo',
       ['@babel/preset-env', {targets: {node: 'current'}}],
       '@babel/preset-typescript',
     ],
