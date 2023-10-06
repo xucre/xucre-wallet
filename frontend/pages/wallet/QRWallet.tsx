d556b9974dbd1488ad8c994266ae2738e53f38fe
Jordan Paul <jordan@getpixeltag.com>
2023-10-05 21:22:42 -0700

    merging whatsappFix branch

--- frontend/pages/wallet/QRWallet.tsx
+++ frontend/pages/wallet/QRWallet.tsx
@@ -58,7 +58,6 @@ export default function QRWallet({ navigation, route }) {
     getPermission();
   }, [isFocused]);
   const getPermission = () => {
-    console.log('_wallet :::', _wallet)
     PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
       title: 'Contacts',
       // eslint-disable-next-line sort-keys
